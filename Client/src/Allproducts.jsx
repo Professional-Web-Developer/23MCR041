70472dcab3c80d75a557f8bc72feaf5d8faa4ca3
kavin <pasupathy.kavinmca@gmail.com>
2024-06-18 13:10:04 +0530

    Frontend  logic

---
+++ Client/src/Allproducts.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import classes from './Allproducts.module.css'
+import axios from 'axios'
+const Allproducts=()=>{
+    const [productname,setProductname]=useState('')
+    const [company,setcompany]=useState('');
+    const [category,setcategory]=useState('')
+    const [price,setprice]=useState('')
+    const [Rating,setrating]=useState('')
+    const [Discount,setdiscount]=usestate('')
+    const [availability,setavail]=useState('')
+    return(
+        <div className={classes.main}>
+            <div className={classes.card}>
+
+
+            </div>
+
+        </div>
+    )
+}
+export default Allproducts;
